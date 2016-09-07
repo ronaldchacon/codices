360302b1bf6b67072625d37a5f015ffd7efa3f1d
Ronald Chacon <ronald.chacon87@gmail.com>
2016-09-06 18:19:10 -0700

    Add Spec/Support Helpers module Update request specs

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe "Books", type: :request do
 
   let(:books) { [ruby_microscope, rails_tutorial, agile_web_dev] }
 
-  let(:json_body) { JSON.parse(response.body) }
-
   describe "GET /api/books" do
     before(:each) { books }
 
