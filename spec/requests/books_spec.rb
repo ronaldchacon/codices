e578d9a590f66c90b6e1e633f5e068dd92646d6c
Ronald Chacon <ronald.chacon87@gmail.com>
2016-09-06 18:05:56 -0700

    Add Books controller Tests passing Add seed data

---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "Books", type: :request do
+  let(:ruby_microscope) { create(:ruby_microscope) }
+  let(:rails_tutorial) { create(:ruby_on_rails_tutorial) }
+  let(:agile_web_dev) { create(:agile_web_development) }
+
+  let(:books) { [ruby_microscope, rails_tutorial, agile_web_dev] }
+
+  let(:json_body) { JSON.parse(response.body) }
+
+  describe "GET /api/books" do
+    before(:each) { books }
+
+    context "default behavior" do
+      before(:each) { get "/api/books" }
+
+      it "gets HTTP status 200" do
+        expect(response.status).to eq 200
+      end
+
+      it "receives a json object with the 'data' root key" do
+        expect(json_body["data"]).to_not be nil
+      end
+
+      it "receives all 3 books" do
+        expect(json_body["data"].size).to eq 3
+      end
+    end
+  end
+end
