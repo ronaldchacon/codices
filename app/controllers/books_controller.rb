aad8fb3a5cf8165bd9244c0e51fb0eb6c4b2d5c7
Ronald Chacon <ronald.chacon87@gmail.com>
2016-11-29 14:13:44 -0800

    Implement serializer for json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   def index
-    books = orchestrate_query(Book.all).map do |book|
-      BookPresenter.new(book, params).fields.embeds
-    end
-
-    render json: { data: books }
+    books = orchestrate_query(Book.all)
+    serializer = Codices::Serializer.new(data: books,
+                                         params: params,
+                                         actions: [:fields, :embeds])
+    render json: serializer.to_json
   end
 end
--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -38,6 +38,11 @@ class BasePresenter
     @data
   end
 
+  def build(actions)
+    actions.each { |action| send(action) }
+    self
+  end
+
   def fields
     FieldPicker.new(self).pick
   end
