7b8e82d7fbcf7fc2fd22d210c7f41fdea6494d35
Ronald Chacon <ronald.chacon87@gmail.com>
2016-11-30 00:42:13 -0800

    Implement show/create/update/destroy actions for books controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,13 @@ class ApplicationController < ActionController::API
   def resource_not_found
     render(status: 404)
   end
+
+  def unprocessable_entity!(resource)
+    render status: :unprocessable_entity, json: {
+      error: {
+        message: "Invalid parameters for resource #{resource.class}.",
+        invalid_params: resource.errors,
+      },
+    }
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,27 @@ class BooksController < ApplicationController
     render serialize(book)
   end
 
+  def create
+    if book.save
+      render serialize(book).merge(status: :created, location: book)
+    else
+      unprocessable_entity!(book)
+    end
+  end
+
+  def update
+    if book.update(book_params)
+      render serialize(book).merge(status: :ok)
+    else
+      unprocessable_entity!(book)
+    end
+  end
+
+  def destroy
+    book.destroy
+    render status: :no_content
+  end
+
   private
 
   def book
@@ -15,4 +36,9 @@ class BooksController < ApplicationController
   end
   alias_method :resource, :book
 
+  def book_params
+    params.require(:data).permit(:title, :subtitle, :isbn_10, :isbn_13,
+                                 :description, :released_on, :publisher_id,
+                                 :author_id, :cover)
+  end
 end
